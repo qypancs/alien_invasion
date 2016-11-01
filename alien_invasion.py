cbebd3f14a6d34ce86c37c61bcbb0ebc53ad1090
qypan <panqingyunzh@gmail.com>
2016-11-01 22:06:04 +0800

    完成飞船和子弹类 完成飞船移动 完成飞船发射子弹，并且添加设置类，完成飞船和子弹以及屏幕的设置

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,27 +1,30 @@
-import sys
 import pygame
+
+from settings import Settings
+from ship import Ship
+from pygame.sprite import Group
+import game_functions as gf
 
 def run_game():
     # 初始化游戏并且创建一个游戏对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 设置背景色
-    bg_color = (230, 230, 230)
+    # 放置一艘飞船
+    ship = Ship(ai_settings, screen)
 
+    bullets = Group()
     # 开始游戏地主循环
     while True:
 
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
 
         # 每次循环时都重绘屏幕
-        screen.fill(bg_color)
-
-        # 让最近绘制地屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
